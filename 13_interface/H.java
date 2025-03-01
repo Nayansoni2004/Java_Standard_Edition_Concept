a59b7c9316235c7bb367d46b79932d6d13508cee
NAYAN_SONI <nayansoni142004@gmail.com>
2025-03-02 00:05:03 +0530

    interfaces methods are implicitely  public and abstract

---
+++ 13_interface/H.java
@@ -0,0 +1,12 @@
+interface H {
+    
+    //method member
+    void pro();
+
+}
+/* happily compiled!!!
+
+interface m kewal method declaration hota h,kyuki interface (100%abstract) class h,
+or interface ki methods implicitely public or abstract hoti hain,mark krne ki jarurat nhi hoti.
+
+*/
