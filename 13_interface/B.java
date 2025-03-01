9013cedf753c2b7acf6259fccdfcfeaf2767cdbf
NAYAN_SONI <nayansoni142004@gmail.com>
2025-03-01 23:48:23 +0530

    method cannot be created directly inside java source file

---
+++ 13_interface/B.java
@@ -0,0 +1,14 @@
+void pro() {    //method declaration cum definition
+
+}
+
+/* B.java:1: error: class, interface, enum, or record expected
+void pro() {    //method declaration cum definition
+^
+1 error
+
+java m data(variables or methods) direct .java source file m nhi rakh sakte,kyuki OOP principles k khilaf h,
+OOP m data or data pr hone wale operations ko special concept:-Data Encapsulation ka use kr k 1 single unit m
+data ko rakha jata h,taki wo massive data secure,accessible ho,wo single unit class,interface,enum ho sakta h...
+or kyuki in teeno m se is .java source file m koi 1 bhi nhi banaya h,isley error aai!!!
+*/
