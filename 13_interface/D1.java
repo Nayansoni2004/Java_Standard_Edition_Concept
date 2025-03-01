d38a2c57d476b3158b0a45b5f30fe0778ee19485
NAYAN_SONI <nayansoni142004@gmail.com>
2025-03-01 23:56:35 +0530

    interface can be marked public if & only if, its name exactly matches the source file name

---
+++ 13_interface/D1.java
@@ -0,0 +1,14 @@
+public interface WWW {
+
+
+}
+
+/* D1.java:1: error: interface WWW is public, should be declared in a file named WWW.java
+public interface WWW {
+       ^
+1 error
+
+jub (interface)or (.java) source file ka nam alag-alag ho to interface ko public mark nhi kr sakte, WWW interface
+ko (WWW.java) source file m hi bs public mark kr sakte h.matlab kewal us interface ko public mark kr sakte h,jis
+interface ka name .java source file k name se match kr rha ho.
+*/  
