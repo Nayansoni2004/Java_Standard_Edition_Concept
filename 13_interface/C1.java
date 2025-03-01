7e331e134a1f5958e73f952b069a0fe0166ac07c
NAYAN_SONI <nayansoni142004@gmail.com>
2025-03-01 23:52:56 +0530

    interfaces have only method declaration not definition

---
+++ 13_interface/C1.java
@@ -0,0 +1,18 @@
+interface X {
+
+
+}
+
+class Y {
+
+
+}
+
+interface Z {
+
+}
+/* OUTPUT:   happily compiled...
+
+interface bhi 1 tarah ka class h,isley interface ko compile krne se bhi(.class) name se file banti h.
+bs interface m kewal or kewal method declaration hota h,1 bhi method definition nhi hoti.
+*/
