46d55b3e7e2f3617f76cf18b5e5b0b9e13e35a40
NAYAN_SONI <nayansoni142004@gmail.com>
2025-03-01 23:42:39 +0530

    data(variables or methods)cannot be created direclty inside java source file

---
+++ 13_interface/A.java
@@ -0,0 +1,14 @@
+int y = 100; //variable declaration cum initialization....
+
+/* A.java:1: error: class, interface, enum, or record expected
+int y = 100; //variable declaration cum initialization....
+^
+1 error
+
+java m data(variables or methods) direct .java source file m nhi rakh sakte,kyuki OOP principles k khilaf h,
+OOP m data or data pr hone wale operations ko special concept:-Data Encapsulation ka use kr k 1 single unit m
+data ko rakha jata h,taki wo massive data secure,accessible ho,wo single unit class,interface,enum ho sakta h...
+or kyuki in teeno m se is .java source file m koi 1 bhi nhi banaya h,isley error aai!!!
+*/
+
+
