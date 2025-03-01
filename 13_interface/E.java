3240c290a5b38717b2ca9633b6014db28c58222e
NAYAN_SONI <nayansoni142004@gmail.com>
2025-03-01 23:59:14 +0530

    interface name exactly matches the source file name

---
+++ 13_interface/E.java
@@ -0,0 +1,8 @@
+public interface E {
+
+}
+/* happily compiled... 
+
+interface ka name or .java source file ka name exact match kr rha h isley interface ko public mark kr sakte,legal
+h,kewal us interface ko public mark kr sakte h,jis interface ka name .java source file k name se match krta ho.
+*/
