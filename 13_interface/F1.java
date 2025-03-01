df1f9910ee7f192f2efa5b9083a9955890c9ef92
NAYAN_SONI <nayansoni142004@gmail.com>
2025-03-02 00:02:54 +0530

    modifier protected is not applicable on interface because then sub-classes cannot be able to implement the interface marked as protected

---
+++ 13_interface/F1.java
@@ -0,0 +1,15 @@
+protected interface F1 {
+
+}
+/* F1.java:1: error: modifier protected not allowed here
+protected interface F1 {
+          ^
+1 error
+
+interface ko kewal public mark kr sakte h,protected mark nhi kr sakte kyuki protected mark kr dene se interface
+sub-classes k instances ya instances k instances me accessible nhi hoga,agar subclass us interface ko inherit 
+krta h toh bhi accessible nhi hoga,yani subclass k bahar wo accessible nhi hoga,lekin subclass k instances ya
+instances k instances me accessible hoga.
+interface ko protected mark kr dene se sub-classes us interface ko implement nhi kr payegi,isley protected 
+mark nhi kr sakte!!!
+*/ 
