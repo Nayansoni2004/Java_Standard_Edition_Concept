bdef36f3d4f7e7c3ff1b8ad4acb534d538298fbb
NAYAN_SONI <nayansoni142004@gmail.com>
2025-03-02 00:00:34 +0530

    modifier private is not applicable on interface

---
+++ 13_interface/F.java
@@ -0,0 +1,12 @@
+private interface F {
+
+}
+/* F.java:1: error: modifier private not allowed here
+private interface F {
+        ^
+1 error
+
+interface ko private mark nhi kr sakte,illegal h,kyuki private mark kr dene se classes interface ko access nhi kr 
+payengi or access hi nhi hoga to methods ko definition bhi provide nhi kr payengi....interface ka purpose hota h
+ki interface public mark ho taki classes use implement kr sakein.
+*/
