06be83c7802151df1305431fae270fcd580ff014
NAYAN_SONI <nayansoni142004@gmail.com>
2025-03-02 22:33:41 +0530

    no need interfaces method as abstract, its work of complier

---
+++ 13_interface/H1.java
@@ -0,0 +1,10 @@
+abstract interface H1 {
+
+    //method member...
+    abstract void update();  //only method declaration...
+
+}
+
+/* happily compiled....lekin interface or interface k methods ko abstract mark krna nhi padta,kyuki interface 
+or interface k methods bydefault abstract mark hote h.inhe compiler automatic abstract mark kr deta h.
+*/
