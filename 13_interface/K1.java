cdec64917d54c522d1457fff8ab481fd600308dd
NAYAN_SONI <nayansoni142004@gmail.com>
2025-03-04 23:21:07 +0530

    interface variables are initialized at the time of declaration

---
+++ 13_interface/K1.java
@@ -0,0 +1,14 @@
+interface K1 {
+    int x = 2;
+}
+
+/*
+ happily compiled...
+ 
+PS D:\javapractice\13_interface> javac K1.java
+PS D:\javapractice\13_interface> java K1
+Error: Main method not found in class K1, please define the main method as:
+   public static void main(String[] args)
+or a JavaFX application class must extend javafx.application.Application
+
+ */
