d9e6b2b09b8d36bbc6b8e46fe117e8729249cc18
NAYAN_SONI <nayansoni142004@gmail.com>
2025-03-05 14:50:52 +0530

    interfaces have only method declaration not defination.

---
+++ 13_interface/O.java
@@ -0,0 +1,14 @@
+interface O {
+    void pro() {
+
+    }
+}
+/*
+interfaces have only method declaration not defination.
+
+PS D:\javapractice\13_interface> javac O.java
+O.java:2: error: interface abstract methods cannot have body
+    void pro() {
+               ^
+1 error
+ */
