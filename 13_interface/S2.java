3eeb8fb488ebd4cf9359400c15ef79a0acd5d819
NAYAN_SONI <nayansoni142004@gmail.com>
2025-03-07 22:51:27 +0530

    a class can implements a interface or a number of interfaces, that is java does not support multiple class inheritance ya but supports multiple inheritance using interfaces.

---
+++ 13_interface/S2.java
@@ -0,0 +1,18 @@
+class A implements D { }
+
+class B { }
+
+class C { }
+
+interface D { }
+
+interface E { }
+
+interface F { }
+
+/*
+happily complied...
+
+a class can implements a interface or a number of interfaces, that is java does not support multiple class
+inheritance ya but supports multiple inheritance using interfaces.
+ */
