48bb8908baa4b230822029961f73d4d9aa400e6e
NAYAN_SONI <nayansoni142004@gmail.com>
2025-03-07 22:53:43 +0530

    a class can implements multiple interfaces that is it supports multiple inheritance for interfaces only.

---
+++ 13_interface/S3.java
@@ -0,0 +1,17 @@
+class A implements D, E { }
+
+class B { }
+
+class C { }
+
+interface D { }
+
+interface E { }
+
+interface F { }
+
+/*
+happily compiled...
+
+a class can implements multiple interfaces that is it supports multiple inheritance for interfaces only.
+ */
