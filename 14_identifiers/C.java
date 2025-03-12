bce0dc24f3a0d57b0febcbb03c94eb6134506503
NAYAN_SONI <nayansoni142004@gmail.com>
2025-03-13 00:04:22 +0530

    Incorrect variable naming: Identifiers cannot start with a digit. Java requires variable names to begin with a letter, _, or $.

---
+++ 14_identifiers/C.java
@@ -0,0 +1,14 @@
+class C {
+    int 2a = 45;
+}
+
+/*
+D:\javapractice\14_identifiers> javac C.java
+C.java:2: error: <identifier> expected
+    int 2a = 45;
+       ^
+C.java:2: error: <identifier> expected
+    int 2a = 45;
+          ^
+2 errors
+ */
