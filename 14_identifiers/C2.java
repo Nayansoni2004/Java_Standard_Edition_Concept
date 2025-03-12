560f77d39d3733008926200048e85b16267feac6
NAYAN_SONI <nayansoni142004@gmail.com>
2025-03-13 00:14:06 +0530

    Declared a valid variable name following Java identifier rules. Identifiers can start with a letter and can include digits after first letter

---
+++ 14_identifiers/C2.java
@@ -0,0 +1,9 @@
+class C2 {
+    int a5675432 = 90;
+}
+/*
+happily compiled...
+
+Declared a valid variable name following Java identifier rules. Identifiers can start with
+a letter and include digits.
+ */
