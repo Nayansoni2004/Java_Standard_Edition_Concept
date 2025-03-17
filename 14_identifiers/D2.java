769df8ab935b5c2485e739916c8026e73757e187
NAYAN_SONI <nayansoni142004@gmail.com>
2025-03-17 23:47:55 +0530

    Attempted to use 'true' as a variable name, but 'true' is a reserved keyword in Java, causing a compilation error

---
+++ 14_identifiers/D2.java
@@ -0,0 +1,13 @@
+class D2 {
+    boolean true = false;
+}
+
+/*
+Attempted to use 'true' as a variable name, but 'true' is a reserved keyword in Java, causing a compilation error.
+
+D:\github_repos\javapractice\14_identifiers> javac D2.java
+D2.java:2: error: <identifier> expected
+    boolean true = false;
+           ^
+1 error
+ */
