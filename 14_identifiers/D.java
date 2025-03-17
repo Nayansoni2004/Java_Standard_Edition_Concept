cd17bbde0ba3b191e8f286ae959748f9313274f9
NAYAN_SONI <nayansoni142004@gmail.com>
2025-03-17 23:46:16 +0530

    we can not use reserved keywords as an identifier.

---
+++ 14_identifiers/D.java
@@ -0,0 +1,19 @@
+class D {
+    int class;
+}
+
+/*
+note: we can not use reserved keywords as an identifier.
+
+D:\github_repos\javapractice\14_identifiers> javac D.java
+D.java:2: error: <identifier> expected
+    int class;
+       ^
+D.java:2: error: <identifier> expected
+    int class;
+             ^
+D.java:3: error: reached end of file while parsing
+ }
+ ^
+3 errors
+*/
