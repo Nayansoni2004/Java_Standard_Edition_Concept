46a5da1c95c09e4ee00bef53b9266c73e4bd0335
NAYAN_SONI <nayansoni142004@gmail.com>
2025-03-17 23:49:50 +0530

    programming component ka name(identifier) ki character limit kitni bhi ho sakti h, that is length matter nhi krti.

---
+++ 14_identifiers/E.java
@@ -0,0 +1,15 @@
+class E {
+    public static void main(String[] args) {
+        int mohanisagoodboyandheismybestfriendheissgoodsinger = 56;
+
+        System.out.println(mohanisagoodboyandheismybestfriendheissgoodsinger);
+    }
+}
+
+/*
+programming component ka name(identifier) ki character limit kitni bhi ho sakti h, that is length matter nhi 
+krti.
+
+happily compiled...
+output: 56
+ */
