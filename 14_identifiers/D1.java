4d2fedd21c9abd4c32f0537cbf561ac19e95f02c
NAYAN_SONI <nayansoni142004@gmail.com>
2025-03-17 23:47:11 +0530

    Used a reserved keyword 'while' as a method name, causing a compilation error,       bec. of voilation of naming rules of a programming component.

---
+++ 14_identifiers/D1.java
@@ -0,0 +1,19 @@
+class D1 {
+    void while() {
+
+    }
+}
+
+/*
+note: Used a reserved keyword 'while' as a method name, causing a compilation error,
+      bec. of voilation of naming rules of a programming component.
+      
+D:\github_repos\javapractice\14_identifiers> javac D1.java
+D1.java:2: error: <identifier> expected
+    void while() {
+        ^
+D1.java:2: error: '(' expected
+    void while() {
+         ^
+2 errors
+ */
