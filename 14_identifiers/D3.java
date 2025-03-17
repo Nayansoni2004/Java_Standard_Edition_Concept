51ef0290615b9ec4e6dff76943dc1cfb3f7effe6
NAYAN_SONI <nayansoni142004@gmail.com>
2025-03-17 23:48:55 +0530

    Because java is case-sensitive that is, it understand the diff. between upper case & lower case, so False is not a reserved keyword in java.

---
+++ 14_identifiers/D3.java
@@ -0,0 +1,10 @@
+class D3 {
+    int False = 2;
+}
+
+/*
+happily compiled...
+
+Because java is case-sensitive that is, it understand the diff. between upper case & lower case, so False is not
+a reserved keyword in java.
+ */
