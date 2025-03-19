dcff3b3b7e3b96baa57f4dae62d099908dbbf815
NAYAN_SONI <nayansoni142004@gmail.com>
2025-03-19 23:40:01 +0530

    Declared two variables 'a' and 'A' to demonstrate Java's case sensitivity, where variables with different cases are treated as distinct

---
+++ 14_identifiers/F1.java
@@ -0,0 +1,11 @@
+class F1 {
+    int a = 8;
+    int A = 9;
+}
+
+/*
+happily compiled...
+
+this time there is no error by declaring two variables with the same name because they are not same, as java is 
+a case sensitive languages i.e. java understands the diff. between upper case and lowercase.
+ */
