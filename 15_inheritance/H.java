7c3f10e53e1da4f1dff36428e62d4572e29bf71c
NAYAN_SONI <nayansoni142004@gmail.com>
2025-03-25 23:15:05 +0530

    java does not support multiple class inheritance

---
+++ 15_inheritance/H.java
@@ -0,0 +1,20 @@
+//Multiple Class Inheritance: (Java does not support multiple class inheritance)
+
+class X2 {
+
+}
+
+class X3 {
+
+}
+
+class H extends X2, X3 {
+
+}
+/*
+D:\github_repos\javapractice\15_inheritance> javac H.java
+H.java:11: error: '{' expected
+class H extends X2, X3 {
+                  ^
+1 error
+ */
