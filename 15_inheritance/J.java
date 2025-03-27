5c6c0845a3c0eefe9cfb56679b7009710dfb42e9
NAYAN_SONI <nayansoni142004@gmail.com>
2025-03-27 23:51:43 +0530

    Demonstrated hierarchical inheritance with multiple child classes (Z1, Z2, Z3) inheriting from a single parent class (J), successfully compiled.

---
+++ 15_inheritance/J.java
@@ -0,0 +1,22 @@
+// Hierarchical Inheritance
+
+class J {
+
+}
+
+class Z1 extends J {
+
+}
+
+class Z2 extends J {
+
+}
+
+class Z3 extends J {
+
+}
+/*
+happily compiled...
+
+one class can have multiple child classes, that's called hierarchical inheritance.
+ */
