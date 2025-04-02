b58fec63f33dabc5bb77c49e89bb5631869246c6
NAYAN_SONI <nayansoni142004@gmail.com>
2025-04-02 23:58:48 +0530

    Verified IS-A relationship using instanceof with SportsCar and Vehicle in polymorphism

---
+++ 16_polymorphism/H1.java
@@ -0,0 +1,15 @@
+class H1 {
+    public static void main(String[] args) {
+        Object x = new SportsCar();
+
+        if(x instanceof Vehicle) {
+            System.out.println("SportsCar IS-A Vehicle");
+        } else {
+            System.out.println("SportsCar IS-not-A Vehicle");
+        }
+    }
+}
+/*
+D:\github_repos\javapractice\16_polymorphism> java H1
+SportsCar IS-A Vehicle
+ */
