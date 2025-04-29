580a5e2f128c8c9b546bbd12e6952fd094e76edc
NAYAN_SONI <nayansoni142004@gmail.com>
2025-04-29 17:28:02 +0530

    entire array object ref. var. is bydefault initialized by null value, and printing it prints null

---
+++ 23_arrays/G.java
@@ -0,0 +1,13 @@
+class G {
+    //array declaration
+    static String[] x; //arr. declaration m galti se bhi size nhi dena h.
+
+    //main method
+    public static void main(String[] args) {
+        System.out.println(x);
+    }
+}
+/* OUTPUT: null
+Entire arr. obj. ref. var. bydefault "null" value se initialize hota h.isley usko print krne pr null print hua.
+explanation in java-copy.
+*/
