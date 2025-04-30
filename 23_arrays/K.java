0fc15579a9f2e2a323446bfd69629587cd1c02db
NAYAN_SONI <nayansoni142004@gmail.com>
2025-04-30 23:57:45 +0530

    Created arrays of different types and observed their reference print behavior

---
+++ 23_arrays/K.java
@@ -0,0 +1,39 @@
+class K {
+    public static void main(String[] args) {
+        boolean[] b = new boolean[3];
+        char[] c = new char[3];
+        byte[] t = new byte[3];
+        short[] s = new short[3];
+        int[] i = new int[3];
+        long[] l = new long[3];
+        float[] f = new float[3];
+        double[] d = new double[3];
+        String[] y = new String[3];
+        Student[] x = new Student[4];
+
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(t);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(y);
+        System.out.println(x);
+    }
+}
+/* OUTPUT
+PS D:\github_repos\javapractice\23_arrays> java K
+[Z@3a71f4dd
+
+[B@7adf9f5f
+[S@85ede7b
+[I@5674cd4d
+[J@63961c42
+[J@63961c42
+[F@65b54208
+[D@1be6f5c3
+[Ljava.lang.String;@6b884d57
+[LStudent;@38af3868
+ */
