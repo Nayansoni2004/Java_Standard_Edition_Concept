93b770922a9ee6066d2c8cecf1e0ec023d4e0a83
NAYAN_SONI <nayansoni142004@gmail.com>
2025-05-01 23:49:03 +0530

    shortcut technique to declare, instantiate & initialize an array on the same line

---
+++ 23_arrays/L1.java
@@ -0,0 +1,15 @@
+class L1 {
+    public static void main(String[] args) {
+        int[] x = new int[]{12, 112}; //shortcut technique : array declaration, arrray instantiation cum initialization.
+
+        System.out.println(x[0]);
+        System.out.println(x[1]);
+    }
+}
+/*
+ShortCut Technique: Array declaration, & instantiation cum initialization.
+
+PS D:\github_repos\javapractice\23_arrays> java L1
+12
+112
+ */
