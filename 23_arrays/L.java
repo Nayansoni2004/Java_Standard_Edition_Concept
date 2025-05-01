6aeb758aed7c04e219b1eaf3381cf6edcb21185c
NAYAN_SONI <nayansoni142004@gmail.com>
2025-05-01 23:20:13 +0530

    Declared and instantiated int array; initialized values later and printed them

---
+++ 23_arrays/L.java
@@ -0,0 +1,16 @@
+class L {
+    public static void main(String[] args) {
+        int[] x = new int[2];
+
+        x[0] = 11;
+        x[1] = 12;
+
+        System.out.println(x[0]);
+        System.out.println(x[1]);
+    }
+}
+/* note: array declaration cum instantiation on the same time and initialization after that.
+PS D:\github_repos\javapractice\23_arrays> java L
+11
+12
+ */
