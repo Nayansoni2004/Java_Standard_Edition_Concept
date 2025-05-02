6fd60b19cc06325102eed18f942c1a1e13bcf003
NAYAN_SONI <nayansoni142004@gmail.com>
2025-05-02 23:51:14 +0530

    shortcut technique in two steps of instantiating cum initializing array of float

---
+++ 23_arrays/M1.java
@@ -0,0 +1,17 @@
+class M1 {
+    public static void main(String[] args) {
+        float[] x; //array declaration
+
+        x = new float[] {1.2f, 2.7f, 9.2f}; // array instantiation cum initialization shortcut technique.
+
+        System.out.println(x[0]);
+        System.out.println(x[1]);
+        System.out.println(x[2]);
+    }
+}
+/*
+OUTPUT
+1.2
+2.7
+9.2
+ */
