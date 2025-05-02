763c9a6644cf22c65c405b92114e4f0bd2e0b4b3
NAYAN_SONI <nayansoni142004@gmail.com>
2025-05-02 23:46:55 +0530

    shortcut technique of array declaration cum instantiation cum initialization

---
+++ 23_arrays/M.java
@@ -0,0 +1,15 @@
+class M {
+    public static void main(String[] args) {
+        float[] x = new float[]{3.4f, 5.6f, 9.8f};
+
+        System.out.println(x[0]);
+        System.out.println(x[1]);
+        System.out.println(x[2]);
+    }
+}
+/*
+OUTPUT
+3.4
+5.6
+9.8
+ */
