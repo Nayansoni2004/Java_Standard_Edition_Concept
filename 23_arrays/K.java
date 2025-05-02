50cfa2d5905cb37a0c33d37b44ca3c7243e440fc
NAYAN_SONI <nayansoni142004@gmail.com>
2025-05-02 23:36:45 +0530

    Add demonstration of array default values and char array printing behavior in class K

--- 23_arrays/K.java
+++ 23_arrays/K.java
@@ -9,7 +9,7 @@ class K {
         float[] f = new float[3];
         double[] d = new double[3];
         String[] y = new String[3];
-        Student[] x = new Student[4];
+        Student[] x = new Student[4]; //This class resolves its exists in H.java
 
         System.out.println(b);
         System.out.println(c);
@@ -21,9 +21,20 @@ class K {
         System.out.println(d);
         System.out.println(y);
         System.out.println(x);
+
+        c[0] = 'A';
+        c[1] = 'B';
+        c[2] = 'V';
+
+        System.out.println(c);
     }
 }
-/* OUTPUT
+/* 
+output gives gap in second line while printing array of char object's ref. code because when we print the ref. code
+of char array object then it implicitly prints elements inside it but, in line second it is not initialized and getting
+print so it prints space and after initializing char array and printing the ref. code print its elements.
+
+OUTPUT
 PS D:\github_repos\javapractice\23_arrays> java K
 [Z@3a71f4dd
 
@@ -31,9 +42,9 @@ PS D:\github_repos\javapractice\23_arrays> java K
 [S@85ede7b
 [I@5674cd4d
 [J@63961c42
-[J@63961c42
 [F@65b54208
 [D@1be6f5c3
 [Ljava.lang.String;@6b884d57
 [LStudent;@38af3868
+ABV
  */
