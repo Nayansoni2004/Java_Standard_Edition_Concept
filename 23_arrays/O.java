cd61bec86e405d33160ebff83bb6abada899389d
NAYAN_SONI <nayansoni142004@gmail.com>
2025-05-03 23:58:36 +0530

    super shortcut technique of array declaration cum instantiation cum initialization on array of string

---
+++ 23_arrays/O.java
@@ -0,0 +1,25 @@
+class O {
+    public static void main(String[] args) {
+        String[] x = {"om", "ram", "shyam", "ghanshyam", "mohan", "rohan", "sohan", "hanuman"}; //super shortcut technique of array declaration cum instantiation cum initialization.
+
+        System.out.println(x[0]);
+        System.out.println(x[1]);
+        System.out.println(x[2]);
+        System.out.println(x[3]);
+        System.out.println(x[4]);
+        System.out.println(x[5]);
+        System.out.println(x[6]);
+        System.out.println(x[7]);
+    }
+}
+/*
+OUTPUT:
+om
+ram
+shyam
+ghanshyam
+mohan
+rohan
+sohan
+hanuman
+ */
