938c12de2c05e996d35692fa9fe12379e6253012
NAYAN_SONI <nayansoni142004@gmail.com>
2025-05-04 17:43:46 +0530

    assigned same class type Animal objects ref. var. into array of type Animal, so code compiles with no errors

---
+++ 23_arrays/S.java
@@ -0,0 +1,18 @@
+class S {
+    public static void main(String[] args) {
+        Animal a1 = new Animal();
+        Animal a2 = new Animal();
+        Animal a3 = new Animal();
+
+        Animal[] x = {a1, a2, a3}; //assigning same class Animal type objects.
+
+        System.out.println(x[0]);
+        System.out.println(x[1]);
+        System.out.println(x[2]);
+    }
+}
+/*OUTPUT
+Animal@85ede7b
+Animal@5674cd4d
+Animal@63961c42
+ */
