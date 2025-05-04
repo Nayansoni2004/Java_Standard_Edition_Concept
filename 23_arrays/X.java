2fe80d89c908119d557ecc6cf601bd6a80d5721a
NAYAN_SONI <nayansoni142004@gmail.com>
2025-05-04 19:16:58 +0530

    sum of entire array elements is 100 in x

---
+++ 23_arrays/X.java
@@ -0,0 +1,16 @@
+class X {
+    public static void main(String[] args) {
+        int[] x = {10, 20, 30, 40, 50};
+
+        int sum = 0;
+
+        for(int i = 0 ; i < x.length ; i++) {
+            sum = sum + x[i];
+        }
+
+        System.out.println(sum);
+    }
+}
+/*OUTPUT
+150
+ */
