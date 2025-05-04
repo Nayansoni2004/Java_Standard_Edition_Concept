2c788587ffcf702394c5b2dad9298d13697cd22d
NAYAN_SONI <nayansoni142004@gmail.com>
2025-05-04 18:16:59 +0530

    done

---
+++ 23_arrays/U.java
@@ -0,0 +1,19 @@
+class U {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        Cow cow = new Cow();
+        Dog dog = new Dog();
+        BDog bdog = new BDog();
+
+        Animal[] x = {cat, cow, dog, bdog}; //no explicit conversion needed because these are sub-types of Animal.
+
+        System.out.println(x[0]);
+        System.out.println(x[1]);
+        System.out.println(x[2]);
+    }
+}
+/*OUTPUT
+Cat@1be6f5c3
+Cow@6b884d57
+Dog@38af3868
+ */
