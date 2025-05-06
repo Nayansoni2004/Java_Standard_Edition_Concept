03821a12078ac75bfe5389508fcf75afbe0eb42a
NAYAN_SONI <nayansoni142004@gmail.com>
2025-05-06 23:12:38 +0530

    second example

---
+++ 24_arrays/B.java
@@ -0,0 +1,7 @@
+class A {
+    public static void main(String[] args) {
+        int[] x = {10, 51, 17, 23, 43}; //super shortcut technique: array declaration cum instantiation cum initialization. 
+
+        System.out.println("Jai Hind");
+    }
+}
