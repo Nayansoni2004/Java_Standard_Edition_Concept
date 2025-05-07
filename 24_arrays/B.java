a088898a8fd0941af79f2293809ddffd911f9f02
NAYAN_SONI <nayansoni142004@gmail.com>
2025-05-07 23:05:08 +0530

    printed elements of array(y) using enhanced for loop(for-each loop).

--- 24_arrays/B.java
+++ 24_arrays/B.java
@@ -1,7 +1,22 @@
-class A {
+class B {
     public static void main(String[] args) {
-        int[] x = {10, 51, 17, 23, 43}; //super shortcut technique: array declaration cum instantiation cum initialization. 
+        int[] y = {10, 51, 17, 23, 43}; //super shortcut technique: array declaration cum instantiation cum initialization. 
 
-        System.out.println("Jai Hind");
+        // for(int i = 0 ; i < y.length ; i++) {
+        //     System.out.println(y[i]);
+        // }
+
+        // Enhanced for loop or for-each loop
+        for(int next : y) {
+            System.out.println(next);
+        }
     }
 }
+/* printed elements of array(y) using enhanced for loop(for-each loop).
+OUTPUT
+10
+51
+17
+23
+43
+ */
