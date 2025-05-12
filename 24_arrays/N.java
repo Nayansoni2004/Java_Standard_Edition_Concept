7f9f60659896579b8f306cecb75b5254dd2e2f6f
NAYAN_SONI <nayansoni142004@gmail.com>
2025-05-12 12:15:39 +0530

    OUTPUT: printed 1d inner arrays objects ref. code using for loop.

---
+++ 24_arrays/N.java
@@ -0,0 +1,13 @@
+class N {
+    public static void main(String[] args) {
+        int[][] x = new int[2][4];
+
+        for(int i = 0 ; i < x.length ; i++) {
+            System.out.println(x[i]);
+        }
+    }
+}
+/* OUTPUT: printed 1d inner arrays objects ref. code using for loop.
+[I@3a71f4dd <-1st 1d inner array
+[I@7adf9f5f <-2nd 1d inner array
+ */
