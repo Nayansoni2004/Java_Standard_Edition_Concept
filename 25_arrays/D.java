b44019f0546d2b94cfd068c1ad805eac0ff52617
NAYAN_SONI <nayansoni142004@gmail.com>
2025-05-17 19:16:28 +0530

    pending

---
+++ 25_arrays/D.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class D {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+
+        int n = sc.nextInt();
+        String[] names = new String[];
+    }
+}
