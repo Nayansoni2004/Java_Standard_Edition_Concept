43b7a287df4d32f2c311a56b643f03b44add304d
NAYAN_SONI <nayansoni142004@gmail.com>
2025-05-20 23:55:52 +0530

    still pending

--- 25_arrays/D.java
+++ 25_arrays/D.java
@@ -7,6 +7,12 @@ class D {
         System.out.print("Enter a number: ");
 
         int n = sc.nextInt();
-        String[] names = new String[];
+        String[] names = new String[n];
+
+        sc.nextLine();
+
+        for(int i = 0 ; i < n ; i++) {
+            System.out.print("Enter name: ");
+        }
     }
 }
