31aff129ffcd7b95f51e01114ffa9e232b3330ae
NAYAN_SONI <nayansoni142004@gmail.com>
2025-05-21 21:46:58 +0530

    took a number as an input from user and created a array of type string whose size is that user input value 'n'    and stored the user input strings in names array and later printed elements of an array names

--- 25_arrays/D.java
+++ 25_arrays/D.java
@@ -9,10 +9,34 @@ class D {
         int n = sc.nextInt();
         String[] names = new String[n];
 
-        sc.nextLine();
+        sc.nextLine(); //this reads the next line that is \n from buffer which was left after pressing the enter key...mandatory line
 
         for(int i = 0 ; i < n ; i++) {
             System.out.print("Enter name: ");
+
+            names[i] = sc.nextLine(); //this reads the next "name" that is next input string from user
         }
+
+        System.out.println("##################################");
+
+        //printing the elements of names array using enhanced for loop(for-each loop)
+        for(String name : names) {
+            System.out.println(name);
+        }
+        
     }
 }
+
+/* took a number as an input from user and created a array of type string whose size is that user input value 'n'
+   and stored the user input strings in names array and later printed elements of an array names
+OUTPUT
+PS D:\github_repos\javapractice\25_arrays> java D
+Enter a number: 3
+Enter name: raju
+Enter name: rancho
+Enter name: farhan
+##################################
+raju
+rancho
+farhan
+ */
