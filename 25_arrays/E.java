2ff0233c5ef21c261d7416569e0199e7b345709f
NAYAN_SONI <nayansoni142004@gmail.com>
2025-05-23 00:11:39 +0530

    Declared an array of 3 Student objects, printed default null values, took name and age input for each student using Scanner, stored them in the array, and finally displayed all student details

--- 25_arrays/E.java
+++ 25_arrays/E.java
@@ -6,12 +6,14 @@ class E {
 
         Student[] x = new Student[3]; //array declaration cum instantiation
 
+        //for-each loop
         for(Student s : x) {
             System.out.print(s + " "); //accessing & printing elements of an array without initialization
         }
 
         System.out.println("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 
+        //classic for loop
         for(int i = 0 ; i < x.length ; i++) {
             System.out.print("Enter name: ");
             String name = sc.nextLine();
@@ -20,6 +22,32 @@ class E {
             int age = sc.nextInt();
 
             x[i] = new Student(name, age);
+            sc.nextLine(); // to consume leftover \n after nextInt()
         }
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+        //for-each loop
+        for(Student s : x) {
+            System.out.println(s); //printing elements of an array x after initialization
+        }
+
+        sc.close();
     }
 }
+/* sc.nextLine() lagana zaruri hai kyunki nextInt() ke baad \n buffer me reh jata hai jo agla nextLine() 
+   read kar leta hai (junk me).
+OUTPUT
+PS D:\github_repos\javapractice\25_arrays> java E
+null null null 
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Enter name: raja
+Enter age: 10
+Enter name: baja
+Enter age: 16
+Enter name: taja
+Enter age: 13
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+raja - 10
+baja - 16
+taja - 13
+ */
