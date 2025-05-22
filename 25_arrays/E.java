d54278a2998d92963a81ebd4e9d168b00b6b41be
NAYAN_SONI <nayansoni142004@gmail.com>
2025-05-22 23:50:42 +0530

    still not done

--- 25_arrays/E.java
+++ 25_arrays/E.java
@@ -1,1 +1,25 @@
-hii this is class keyword
+import java.util.Scanner;
+
+class E {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Student[] x = new Student[3]; //array declaration cum instantiation
+
+        for(Student s : x) {
+            System.out.print(s + " "); //accessing & printing elements of an array without initialization
+        }
+
+        System.out.println("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+        for(int i = 0 ; i < x.length ; i++) {
+            System.out.print("Enter name: ");
+            String name = sc.nextLine();
+
+            System.out.print("Enter age: ");
+            int age = sc.nextInt();
+
+            x[i] = new Student(name, age);
+        }
+    }
+}
