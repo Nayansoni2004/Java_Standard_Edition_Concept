35249f2d349ead791fcf5210e405990bef0c45b9
NAYAN_SONI <nayansoni142004@gmail.com>
2025-05-25 23:30:06 +0530

    re-initialization

---
+++ 25_arrays/H.java
@@ -0,0 +1,11 @@
+public class H {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = a;
+        a = 3;
+        int c =  2 * (a * b);
+        
+        System.out.println(c);
+        System.out.println((a == b));
+    }
+}
