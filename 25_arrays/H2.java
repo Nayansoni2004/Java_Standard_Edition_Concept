39ea3ed8ae751fa637d629a8079ba41ade48d385
NAYAN_SONI <nayansoni142004@gmail.com>
2025-05-27 00:19:49 +0530

    this is correct version

---
+++ 25_arrays/H2.java
@@ -0,0 +1,8 @@
+class H2 {
+    public static void main(String[] args) {
+        String[][] x = { {"om"}, {"ram"}, {"shyam"} };//created 2d-array of size 3
+    }
+}
+/* code compiles successfully...
+this is correct version
+ */
