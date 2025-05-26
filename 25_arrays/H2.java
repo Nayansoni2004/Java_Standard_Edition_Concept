fa93767d763fc5637ce3e32e106e09d80447f9e1
NAYAN_SONI <nayansoni142004@gmail.com>
2025-05-27 00:23:32 +0530

    created 2d array using super-shortcut technique and initialized 1d-arrays of length each 1

--- 25_arrays/H2.java
+++ 25_arrays/H2.java
@@ -1,6 +1,10 @@
 class H2 {
     public static void main(String[] args) {
         String[][] x = { {"om"}, {"ram"}, {"shyam"} };//created 2d-array of size 3
+
+        for(int i = 0 ; i < x.length ; i++) {
+            System.out.println(i + " - " + x[i].length);
+        }
     }
 }
 /* code compiles successfully...
