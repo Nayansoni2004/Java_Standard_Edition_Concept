ccfe9e32e9d7773180d3a0009ab8df7efab65756
NAYAN_SONI <nayansoni142004@gmail.com>
2025-05-27 00:14:47 +0530

    H1.java:3: error: incompatible types: String cannot be converted to String[]

--- 25_arrays/H1.java
+++ 25_arrays/H1.java
@@ -1,5 +1,20 @@
 class H1 {
     public static void main(String[] args) {
-        String[][] x = {"om"}
+        String[][] x = {"om", "ram", "shyam"};//declared 2d array & initializing using super-shortcut technique, direct 1d array.
     }
 }
+/* compilation error... bec. left side declared 2d array & right side initializing 1d-array so outer 2d-array size is not defined,
+  so that compilation fails...
+  
+PS D:\github_repos\javapractice\25_arrays> javac H1.java
+H1.java:3: error: incompatible types: String cannot be converted to String[]
+        String[][] x = {"om", "ram", "shyam"};
+                        ^
+H1.java:3: error: incompatible types: String cannot be converted to String[]
+        String[][] x = {"om", "ram", "shyam"};
+                              ^
+H1.java:3: error: incompatible types: String cannot be converted to String[]
+        String[][] x = {"om", "ram", "shyam"};
+                                     ^
+3 errors
+ */
