de2c5c8b262a62ebf3474f73d2e30eee2fee0867
NAYAN_SONI <nayansoni142004@gmail.com>
2025-06-02 23:48:16 +0530

    pending task

---
+++ 25_arrays/P.java
@@ -0,0 +1,8 @@
+//Task 2: Create a new 2D array y by reversing each 1D array elements inside the given 2D array x while keeping their positions same.
+public class P {
+    public static void main(String[] args) {
+        int[][] x = {{2, 9, 7, 3, 4}, {5, 8, 11}, {3, 5}, {8}};
+
+        int[][] y = new int[x.length][]; //created 2d array y of same length
+    }    
+}
